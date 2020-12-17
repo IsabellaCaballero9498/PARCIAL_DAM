5a8ada6604befeefbe402ae8f5a8d85fcb85e9d1
Luis Angel Izquierdo <luisangel19992412@gmail.com>
2020-12-17 16:52:58 -0500

    Alumno 2 Listo

--- app/src/main/java/com/dam/dam_parcial/MainActivity.java
+++ app/src/main/java/com/dam/dam_parcial/MainActivity.java
@@ -2,13 +2,26 @@ package com.dam.dam_parcial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ImageButton btnLogin;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.imageButton5);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Registro.class);
+                startActivity(i);
+            }
+        });
     }
 }
